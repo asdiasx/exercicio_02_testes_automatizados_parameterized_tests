8fc41fd0d3ca5df0a62a6b7368b415d40d0c66f2
Adriano Dias <adriano.rj.br@gmail.com>
2023-05-14 22:42:46 -0300

    final commit

--- src/main/java/org/example/Vote.java
+++ src/main/java/org/example/Vote.java
@@ -4,6 +4,7 @@ public class Vote {
 
 
     public static String canVote(int age) {
+        if (age < 0 || age > 150) throw new IllegalArgumentException("Idade inválida!");
         if (age < 16) return "Sem direito a votar";
         if (age < 18 || age > 70) return "Voto facultativo";
         return "Voto obrigatório";
--- src/test/java/VoteTest.java
+++ src/test/java/VoteTest.java
@@ -1,42 +1,73 @@
+import org.example.Vote;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.example.Vote.canVote;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class VoteTest {
 
+    private static Stream<Arguments> voteConditions() {
+        return Stream.of(
+                Arguments.of(15, "Sem direito a votar"),
+                Arguments.of(16, "Voto facultativo"),
+                Arguments.of(17, "Voto facultativo"),
+                Arguments.of(18, "Voto obrigatório"),
+                Arguments.of(30, "Voto obrigatório"),
+                Arguments.of(70, "Voto obrigatório"),
+                Arguments.of(71, "Voto facultativo"),
+                Arguments.of(80, "Voto facultativo")
+        );
+    }
 
-    @Test
-    void mustVoteTest() {
-        assertEquals("Voto obrigatório", canVote(18));
-        assertEquals("Voto obrigatório", canVote(70));
-        assertEquals("Voto obrigatório", canVote(35));
+    @ParameterizedTest
+    @CsvSource({
+            "15, Sem direito a votar",
+            "16, Voto facultativo",
+            "17, Voto facultativo",
+            "18, Voto obrigatório",
+            "30, Voto obrigatório",
+            "70, Voto obrigatório",
+            "71, Voto facultativo",
+            "80, Voto facultativo"
+    })
+    public void testCanVoteCsvSource(int age, String expected) {
+        String result = Vote.canVote(age);
+        assertEquals(expected, result);
+    }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "dados_teste.csv", numLinesToSkip = 1)
+    public void testCanVoteCsvFile(int age, String expected) {
+        String result = Vote.canVote(age);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("voteConditions")
+    public void testCanVoteMethodSource(int age, String expected) {
+        String result = Vote.canVote(age);
+        assertEquals(expected, result);
     }
 
     @Test
-    void voteOptionalTest() {
-        assertEquals("Voto facultativo", canVote(16));
-        assertEquals("Voto facultativo", canVote(17));
-        assertEquals("Voto facultativo", canVote(71));
-        assertEquals("Voto facultativo", canVote(99));
+    public void testNegativeAgeException() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> Vote.canVote(-1));
+
+        Assertions.assertEquals("Idade inválida!", thrown.getMessage());
     }
 
     @Test
-    void cantVoteTest() {
-        assertEquals("Sem direito a votar", canVote(15));
-        assertEquals("Sem direito a votar", canVote(5));
-        assertEquals("Sem direito a votar", canVote(0));
-        assertEquals("Sem direito a votar", canVote(-1));
-    }
+    public void testTooBigAgeException() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> Vote.canVote(151));
 
-    @Test
-    void mustVoteTestWithAssertAll() {
-        assertAll(
-                () -> assertEquals("Voto obrigatório", canVote(18)),
-                () -> assertEquals("Voto obrigatório", canVote(70)),
-                () -> assertEquals("Voto obrigatório", canVote(35))
-                );
+        Assertions.assertEquals("Idade inválida!", thrown.getMessage());
     }
 }
